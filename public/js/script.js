16eedb30609bec66a3d0ec30daedd225a3da66ae
Mu-jun <suo24691@gmail.com>
2022-09-15 09:11:28 +0900

    엑셀 다운로드 수정

--- public/js/script.js
+++ public/js/script.js
@@ -32,94 +32,35 @@ function del_bbs(id) {
     }
 }
 
-/**
-* 엑셀 다운로드
-* @param fileName 엑셀파일명 (ex. excel.xls)
-* @param sheetName 시트명
-* @param headers 시트내용(html - table)
-*/
-function _excelDown(fileName, sheetName, sheetHtml) {
-    var html = '';
-    html += '<html xmlns:x="urn:schemas-microsoft-com:office:excel">';
-    html += '    <head>';
-    html += '        <meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8">';
-    html += '        <xml>';
-    html += '            <x:ExcelWorkbook>';
-    html += '                <x:ExcelWorksheets>';
-    html += '                    <x:ExcelWorksheet>'
-    html += '                        <x:Name>' + sheetName + '</x:Name>';   // 시트이름
-    html += '                        <x:WorksheetOptions><x:Panes></x:Panes></x:WorksheetOptions>';
-    html += '                    </x:ExcelWorksheet>';
-    html += '                </x:ExcelWorksheets>';
-    html += '            </x:ExcelWorkbook>';
-    html += '        </xml>';
-    html += '    </head>';
-    html += '    <body>';
-    
-    // ----------------- 시트 내용 부분 -----------------
-    html += sheetHtml;
-    // ----------------- //시트 내용 부분 -----------------
-    
-    html += '    </body>';
-    html += '</html>';
+function exportExcel(table_id, fileName, sheetName) {
+    // step 1. workbook 생성
+    var wb = XLSX.utils.book_new();
 
-    console.log(html)
-    // 데이터 타입
-    var data_type = 'data:application/vnd.ms-excel';
-    var ua = window.navigator.userAgent;
-    var blob = new Blob([html], {type: "application/csv;charset=utf-8;"});
-    
-    if ((ua.indexOf("MSIE ") > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) && window.navigator.msSaveBlob) {
-        // ie이고 msSaveBlob 기능을 지원하는 경우
-        navigator.msSaveBlob(blob, fileName);
-    } else {
-        // ie가 아닌 경우 (바로 다운이 되지 않기 때문에 클릭 버튼을 만들어 클릭을 임의로 수행하도록 처리)
-        var anchor = window.document.createElement('a');
-        anchor.href = window.URL.createObjectURL(blob);
-        anchor.download = fileName;
-        document.body.appendChild(anchor);
-        anchor.click();
-        
-        // 클릭(다운) 후 요소 제거
-        document.body.removeChild(anchor);
+    // step 2. 시트 만들기 
+    var newWorksheet = excelHandler.getWorksheet(table_id);
+
+    // step 3. workbook에 새로만든 워크시트에 이름을 주고 붙인다.  
+    XLSX.utils.book_append_sheet(wb, newWorksheet, sheetName);
+
+    // step 4. 엑셀 파일 만들기 
+    var wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+
+    // step 5. 엑셀 파일 내보내기 
+    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), fileName);
+}
+
+var excelHandler = {
+    getExcelData: function (table_id) {
+        return document.getElementById(table_id); 	//TABLE id
+    },
+    getWorksheet: function (table_id) {
+        return XLSX.utils.table_to_sheet(this.getExcelData(table_id));
     }
 }
 
-function exportExcel(title){ 
-    // step 1. workbook 생성
-    var wb = XLSX.utils.book_new();
-  
-    // step 2. 시트 만들기 
-    var newWorksheet = excelHandler.getWorksheet(title);
-  
-    // step 3. workbook에 새로만든 워크시트에 이름을 주고 붙인다.  
-    XLSX.utils.book_append_sheet(wb, newWorksheet, excelHandler.getSheetName());
-  
-    // step 4. 엑셀 파일 만들기 
-    var wbout = XLSX.write(wb, {bookType:'xlsx',  type: 'binary'});
-  
-    // step 5. 엑셀 파일 내보내기 
-    saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), excelHandler.getExcelFileName());
-  }
-  
-  var excelHandler = {
-      getExcelFileName : function(){
-          return 'table-test.xlsx';	//파일명
-      },
-      getSheetName : function(){
-          return 'Table Test Sheet';	//시트명
-      },
-      getExcelData : function(title){
-          return document.getElementById(title); 	//TABLE id
-      },
-      getWorksheet : function(title){
-          return XLSX.utils.table_to_sheet(this.getExcelData(title));
-      }
-  }
-  
-  function s2ab(s) { 
+function s2ab(s) {
     var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
     var view = new Uint8Array(buf);  //create uint8array as viewer
-    for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
-    return buf;    
-  }
+    for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+    return buf;
+}
